289ee13f0c636e5a0a2741d824ae2e76b8b0fc4f
nowa <57222741+nowaaaaaa@users.noreply.github.com>
2023-03-09 11:16:11 +0100

    Update main.py

--- SBOM/main.py
+++ SBOM/main.py
@@ -3,4 +3,11 @@ import PySimpleGUI as sg
 
 layout = [[sg.Text("We gaan SBOMbarderen.")], [sg.Button("OK DOE DAN")]]
 
-sg.Window(title="SBOMGUI", layout=layout, margins=(600, 300)).read()
+window = sg.Window(title="SBOMGUI", layout=layout, margins=(600, 300))
+
+while True:
+    event, values = window.read()
+    if event == "OK DOE DAN" or event == sg.WIN_CLOSED:
+        break
+
+window.close()
