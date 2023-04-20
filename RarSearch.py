8212357fabc717ab2d99b1c1454d94407abcd752
nowa <57222741+nowaaaaaa@users.noreply.github.com>
2023-04-20 15:10:02 +0200

    RarSearch zoekt alleen archives

--- RarSearch.py
+++ RarSearch.py
@@ -11,16 +11,18 @@ def print_all_files_in_docker_container(image_name):
         exec_instance = container.exec_run(cmd="find / -name '*.rar' -o -name '*.zip' -o -name '*.tar' -o -name '*.tar.gz' -o -name '*.jar' -o -name '*.war' -o -name '*.ear'")
         for file_path in exec_instance.output.decode().splitlines():
             print(file_path)
-            #os.system(f"syft {file_path} -o json > {file_path}.json")
-            if file_path.endswith(('.rar', '.zip', '.tar', '.tar.gz', '.jar', '.war', '.ear', '.rar')):
-                try:
-                    with container.get_archive(file_path) as stream:
-                        tar = tarfile.open(fileobj=stream)
-                        for tarinfo in tar:
-                            print(tarinfo.name)
-                        tar.close()
-                except Exception as e:
-                    print(f"Error reading archive {file_path}: {e}")
+            #fileName = os.path.split(file_path)[1]
+            #os.system(f"syft {file_path} -o syft-json --file C:/Users/nootj/Desktop/{fileName}.json")
+            # if file_path.endswith(('.rar', '.zip', '.tar', '.tar.gz', '.jar', '.war', '.ear', '.rar')):
+            #     try:
+            #         with container.get_archive(file_path) as stream:
+            #             tar = tarfile.open(fileobj=stream)
+            #             for tarinfo in tar:
+            #                 print(tarinfo.name)
+            #             tar.close()
+            #     except Exception as e:
+            #         #print(f"Error reading archive {file_path}: {e}")
+            #         pass
     except docker.errors.APIError as e:
         print(e)
     finally:
