a9597a5ca3e1fc73ff3cd58504387dd465961379
nowa <57222741+nowaaaaaa@users.noreply.github.com>
2023-04-25 13:55:42 +0200

    compilet niet maar syft vindt dependency wel

---
+++ OneJar/jarIt/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("commons-math4-core              4.0-beta1  java-archive\n" +
+                "commons-math4-legacy            4.0-beta1  java-archive\n" +
+                "commons-math4-legacy-core       4.0-beta1  java-archive\n" +
+                "commons-math4-legacy-exception  4.0-beta1  java-archive\n" +
+                "commons-math4-neuralnet         4.0-beta1  java-archive\n" +
+                "commons-math4-transform         4.0-beta1  java-archive");
+    }
+}
