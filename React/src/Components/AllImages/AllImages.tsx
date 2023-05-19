a8dba8f53cc1305617ac9cfa73f95b6fd85365a1
kieran20022 <kieranvdheijden@gmail.com>
2023-05-19 20:47:00 +0200

    Image Search

---
+++ React/src/Components/AllImages/AllImages.tsx
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from "react";
+import "../../Pages/Home/App.css";
+import baseline from "../../assets/baseline.json";
+import TextField from "@mui/material/TextField";
+import Tooltip from "@mui/material/Tooltip";
+import Switch from "@mui/material/Switch";
+
+const baseLine = baseline as Dependency[];
+
+interface Dependency {
+  name: string;
+  version: string;
+}
+
+interface Image {
+  name: string;
+  dependencies: Dependency[];
+}
+
+export const AllImages: React.FC<{
+  file: Image[];
+  search: string;
+  depSearch: string;
+}> = ({ file, search, depSearch }) => {
+  return (
+    <div className="AllImages">
+      {file.map((image) => {
+        if (search === "" || image.name.includes(search)) {
+          return <SingleImage image={image} depSearch={depSearch} />;
+        }
+      })}
+    </div>
+  );
+};
+
+const DisplayDep: React.FC<{ dep: Dependency }> = ({ dep }) => {
+  return (
+    <div className="DepHolder">
+      <p className="">{dep.name}</p>
+      <p className="version">{dep.version}</p>
+    </div>
+  );
+};
+
+const SingleImage: React.FC<{ image: Image; depSearch: string }> = ({
+  image,
+  depSearch: allDepSearch,
+}) => {
+  const [imageDepSearch, setSearch] = useState<string>("");
+  const [useBase, setBase] = useState<boolean>(true);
+  return (
+    <div className="SingleImage">
+      <div className="HeaderHolder">
+        <h1 className="ImageHeader">{image.name}</h1>
+        <TextField
+          autoComplete="d"
+          label="Search Dependencies"
+          onChange={(e) => setSearch(e.target.value)}
+          sx={{ height: "50px" }}
+        />
+        <Tooltip title="Filter Baseline">
+          <Switch
+            color="secondary"
+            className="Switch"
+            defaultChecked={useBase}
+            onChange={() => setBase(!useBase)}
+          />
+        </Tooltip>
+      </div>
+      <div className="DepMap">
+        {image.dependencies.map((dep) => {
+          if (!useBase) {
+            if (imageDepSearch !== "" && dep.name.includes(imageDepSearch)) {
+              return <DisplayDep dep={dep} />;
+            } else if (imageDepSearch === "") {
+              if (allDepSearch === "" || dep.name.includes(allDepSearch)) {
+                return <DisplayDep dep={dep} />;
+              }
+            }
+          } else {
+            if (
+              baseLine.some(
+                (baseDep) =>
+                  baseDep.name === dep.name && baseDep.version === dep.version
+              )
+            ) {
+              if (imageDepSearch !== "" && dep.name.includes(imageDepSearch)) {
+                return <DisplayDep dep={dep} />;
+              } else if (imageDepSearch === "") {
+                if (allDepSearch === "" || dep.name.includes(allDepSearch)) {
+                  return <DisplayDep dep={dep} />;
+                }
+              }
+            }
+          }
+        })}
+      </div>
+    </div>
+  );
+};
--- React/src/Pages/Home/Home.tsx
+++ React/src/Pages/Home/Home.tsx
@@ -1,16 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import baseline from "../../assets/baseline.json";
 import { Sidebar } from "../../Components/Sidebar/Sidebar";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import Accordion from "@mui/material/Accordion";
-import AccordionDetails from "@mui/material/AccordionDetails";
 import TextField from "@mui/material/TextField";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import ImageSearchIcon from "@mui/icons-material/ImageSearch";
-import FindInPageIcon from "@mui/icons-material/FindInPage";
-import Tooltip from "@mui/material/Tooltip";
-import Switch from "@mui/material/Switch";
+import { AllImages } from "../../Components/AllImages/AllImages";
 
 interface Dependency {
   name: string;
@@ -22,77 +14,10 @@ interface Image {
   dependencies: Dependency[];
 }
 
-const baseLine = baseline as Dependency[];
-
-const AllImages: React.FC<{ file: Image[] }> = ({ file }) => {
-  return (
-    <div className="AllImages">
-      {file.map((image) => {
-        return <SingleImage image={image} />;
-      })}
-    </div>
-  );
-};
-
-const SingleImage: React.FC<{ image: Image }> = ({ image }) => {
-  const [search, setSearch] = useState<string>("");
-  const [useBase, setBase] = useState<boolean>(true);
-  return (
-    <div className="SingleImage">
-      <div className="HeaderHolder">
-        <h1 className="ImageHeader">{image.name}</h1>
-        <TextField
-          autoComplete="d"
-          label="Search Dependencies"
-          onChange={(e) => setSearch(e.target.value)}
-          sx={{ height: "50px" }}
-        />
-        <Tooltip title="Filter Baseline">
-          <Switch
-            color="secondary"
-            className="Switch"
-            defaultChecked={useBase}
-            onChange={() => setBase(!useBase)}
-          />
-        </Tooltip>
-      </div>
-      <div className="DepMap">
-        {image.dependencies.map((dep) => {
-          if (!useBase) {
-            if (search == "" || dep.name.includes(search)) {
-              return (
-                <div className="DepHolder">
-                  <p className="">{dep.name}</p>
-                  <p className="version">{dep.version}</p>
-                </div>
-              );
-            }
-          } else {
-            if (
-              baseLine.some(
-                (baseDep) =>
-                  baseDep.name === dep.name && baseDep.version === dep.version
-              ) &&
-              (search === "" || dep.name.includes(search))
-            ) {
-              return (
-                <div className="DepHolder">
-                  <p className="">{dep.name}</p>
-                  <p className="version">{dep.version}</p>
-                </div>
-              );
-            }
-          }
-        })}
-      </div>
-    </div>
-  );
-};
-
 export function Home() {
   const [file, setFile] = useState<Image[]>([]);
-  const [search, setSearch] = useState("");
-  const [useBase, setUseBase] = useState<boolean>(false);
+  const [searchImage, setSearchImage] = useState("");
+  const [searchDep, setSearchDep] = useState("");
 
   useEffect(() => {
     fetch(
@@ -100,7 +25,6 @@ export function Home() {
     )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         setFile(data);
       });
   }, []);
@@ -112,44 +36,16 @@ export function Home() {
         <div className="main">
           <h1 className="image-name">Your Images:</h1>
           <div className="textFields">
-            {/* <Tooltip title="Search Dependencies">
-              <span
-                onClick={() => {
-                  fetch(
-                    `https://eu-central-1.aws.data.mongodb-api.com/app/data-xmrsh/endpoint/getDeps?dep=${search}`
-                  )
-                    .then((response) => response.json())
-                    .then((data) => {
-                      console.log(data);
-                      setFile(data);
-                    });
-                }}
-              >
-                <FindInPageIcon fontSize="large" className="icon" />
-              </span>
-            </Tooltip> */}
             <TextField
               label="Search Images"
-              onChange={(e) => setSearch(e.target.value)}
+              onChange={(e) => setSearchImage(e.target.value)}
             />
-            <Tooltip title="Search Images">
-              <span
-                onClick={() => {
-                  fetch(
-                    `https://eu-central-1.aws.data.mongodb-api.com/app/data-xmrsh/endpoint/getDeps?name=${search}`
-                  )
-                    .then((response) => response.json())
-                    .then((data) => {
-                      console.log(data);
-                      setFile(data);
-                    });
-                }}
-              >
-                <ImageSearchIcon fontSize="large" className="icon" />
-              </span>
-            </Tooltip>
+            <TextField
+              label="Search Dependencies"
+              onChange={(e) => setSearchDep(e.target.value)}
+            />
           </div>
-          <AllImages file={file} />
+          <AllImages file={file} search={searchImage} depSearch={searchDep} />
         </div>
       </div>
     </>
