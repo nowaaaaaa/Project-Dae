76be45920018e5d3426f4825c8dda88f69e1ab45
kieran20022 <kieranvdheijden@gmail.com>
2023-05-19 11:51:17 +0200

    sef

--- React/src/Pages/Home/Home.tsx
+++ React/src/Pages/Home/Home.tsx
@@ -24,15 +24,6 @@ interface Image {
 
 const baseLine = baseline as Dependency[];
 
-// const DisplayDep: React.FC<{ dep: Dependency }> = ({ dep }) => {
-//   return (
-//     <div className="dep">
-//       <p className="info">{dep.name}</p>
-//       <p className="info">{dep.version}</p>
-//     </div>
-//   );
-// };
-
 const AllImages: React.FC<{ file: Image[] }> = ({ file }) => {
   return (
     <div className="AllImages">
@@ -103,54 +94,6 @@ export function Home() {
   const [search, setSearch] = useState("");
   const [useBase, setUseBase] = useState<boolean>(false);
 
-  // const DisplayFile: React.FC<{ file: Dependency[] }> = ({ file }) => {
-  //   return (
-  //     <div className="foundDeps">
-  //       {file.map((dep) => {
-  //         if (!useBase) {
-  //           return <DisplayDep dep={dep} />;
-  //         }
-  //         //check if the dependency is in the baseline
-  //         const found = baseLine.find((baseDep) => {
-  //           return baseDep.name === dep.name;
-  //         });
-  //         if (found) {
-  //           //if it is, check if the version is the same
-  //           if (found.version === dep.version) {
-  //             return <DisplayDep dep={dep} />;
-  //           }
-  //         }
-  //       })}
-  //     </div>
-  //   );
-  // };
-
-  // const ImageAccordions: React.FC = () => {
-  //   return (
-  //     <div className="accHolder">
-  //       {file.map((image) => {
-  //         return (
-  //           <>
-  //             <Accordion
-  //               className="acc"
-  //               sx={{ width: "50%", bgcolor: "#eef9f5" }}
-  //             >
-  //               <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-  //                 <div>
-  //                   <h1 className="image-name">{image.name}</h1>
-  //                 </div>
-  //               </AccordionSummary>
-  //               <AccordionDetails>
-  //                 <DisplayFile file={image.dependencies} />
-  //               </AccordionDetails>
-  //             </Accordion>
-  //           </>
-  //         );
-  //       })}
-  //     </div>
-  //   );
-  // };
-
   useEffect(() => {
     fetch(
       "https://eu-central-1.aws.data.mongodb-api.com/app/data-xmrsh/endpoint/getDeps"
@@ -206,14 +149,6 @@ export function Home() {
               </span>
             </Tooltip>
           </div>
-          {/* <div className="check">
-            Filter baseline
-            <Switch
-              defaultChecked={useBase}
-              color="secondary"
-              onChange={() => setUseBase(!useBase)}
-            />
-          </div> */}
           <AllImages file={file} />
         </div>
       </div>
