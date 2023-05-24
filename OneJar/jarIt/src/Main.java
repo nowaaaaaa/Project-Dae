34861562bdcfb320d47379af8a73296fe6763435
nowa <57222741+nowaaaaaa@users.noreply.github.com>
2023-05-24 20:50:44 +0200

    compilen lukt

--- OneJar/jarIt/src/Main.java
+++ OneJar/jarIt/src/Main.java
@@ -10,3 +10,8 @@ public class Main {
                 "commons-math4-transform         4.0-beta1  java-archive");
     }
 }
+//Windows
+//java -cp .;lib/commons-math-4.0-beta1/commons-math4-core-4.0-beta1.jar;lib/commons-math-4.0-beta1/commons-math4-legacy-4.0-beta1.jar;lib/commons-math-4.0-beta1/commons-math4-legacy-core-4.0-beta1.jar;lib/commons-math-4.0-beta1/commons-math4-legacy-exception-4.0-beta1.jar;lib/commons-math-4.0-beta1/commons-math4-neuralnet-4.0-beta1.jar;lib/commons-math-4.0-beta1/commons-math4-transform-4.0-beta1.jar src/Main.java
+//
+//Unix
+//java -cp .:lib/commons-math-4.0-beta1/commons-math4-core-4.0-beta1.jar:lib/commons-math-4.0-beta1/commons-math4-legacy-4.0-beta1.jar:lib/commons-math-4.0-beta1/commons-math4-legacy-core-4.0-beta1.jar:lib/commons-math-4.0-beta1/commons-math4-legacy-exception-4.0-beta1.jar:lib/commons-math-4.0-beta1/commons-math4-neuralnet-4.0-beta1.jar:lib/commons-math-4.0-beta1/commons-math4-transform-4.0-beta1.jar src/Main.java
