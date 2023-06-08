7fdb88a8baedd4a9d6eb548272177c2b00a0d6f6
kieran20022 <kieranvdheijden@gmail.com>
2023-06-08 22:48:54 +0200

    python server
    ik krijg hoofdpijn

--- React/server.py
+++ React/server.py
@@ -44,12 +44,55 @@ def get_dependencies(database, collection, depName, vStart, vEnd):
 
     foundDeps = []
 
+    if ("." in vStart):
+        vSplitS = vStart.split(".")
+    else:
+        vSplitS = False
+    if ("." in vEnd):
+        vSplitE = vEnd.split(".")
+    else:
+        vSplitE = False
+
     for image in collection.find():
         deps = image.get("dependencies")
         foundDeps.append({"dependencies": [], "name": image.get("name")})
         for dep in deps:
             if (dep.get("name") == depName or depName == "any"):
-                if ((vStart <= dep.get("version") <= vEnd) and (vStart != "any" and vEnd != "any")):
+                if ("." in dep.get("version")):
+                    vSplitD = dep.get("version").split(".")
+                    if ("." not in vStart and vEnd == "any" and int(vStart) <= int(vSplitD[0])):
+                        output = {
+                            "name": dep.get("name"),
+                            "version": dep.get("version"),
+                            "purl": dep.get('purl')
+                        }
+                        foundDeps[-1]["dependencies"].append(output)
+                    elif ("." not in vEnd and vStart == "any" and int(vEnd) >= int(vSplitD[0])):
+                        output = {
+                            "name": dep.get("name"),
+                            "version": dep.get("version"),
+                            "purl": dep.get('purl')
+                        }
+                        foundDeps[-1]["dependencies"].append(output)
+                    elif (vSplitS is not False and vSplitE is not False):
+                        if (int(vSplitS[0]) <= int(vSplitD[0]) <= int(vSplitE[0])):
+                            if (len(vSplitS) > 1 and len(vSplitD) > 1 and int(vSplitS[1]) <= int(vSplitD[1]) <= int(vSplitE[1])):
+                                if (len(vSplitS) > 2 and len(vSplitD) > 2 and int(vSplitS[2]) <= int(vSplitD[2]) <= int(vSplitE[2])):
+                                    if (len(vSplitS) > 3 and len(vSplitD) > 3 and int(vSplitS[3]) <= int(vSplitD[3]) <= int(vSplitE[3])):
+                                        output = {
+                                            "name": dep.get("name"),
+                                            "version": dep.get("version"),
+                                            "purl": dep.get('purl')
+                                        }
+                                        foundDeps[-1]["dependencies"].append(output)
+                            elif (len(vSplitS) == 1 and len(vSplitD) == 1):
+                                output = {
+                                    "name": dep.get("name"),
+                                    "version": dep.get("version"),
+                                    "purl": dep.get('purl')
+                                }
+                                foundDeps[-1]["dependencies"].append(output)
+                elif ((vStart <= dep.get("version") <= vEnd) and (vStart != "any" and vEnd != "any")):
                     output = {
                         "name": dep.get("name"),
                         "version": dep.get("version"),
@@ -78,12 +121,6 @@ def get_dependencies(database, collection, depName, vStart, vEnd):
                     }
                     foundDeps[-1]["dependencies"].append(output)
 
-    # for dependency in collection.find():
-    #     output = {
-    #         "name": dependency.get('name'),
-    #         "version": dependency.get('version')
-    #     }
-    #     dependencies.append(output)
 
     return jsonify(foundDeps)
 
