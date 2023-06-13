b2c895e030113f388ef27c99d869c543081d8142
nowa <57222741+nowaaaaaa@users.noreply.github.com>
2023-06-13 13:37:07 +0200

    filestructuur als het goed is beter

--- start.py
+++ start.py
@@ -1,12 +1,11 @@
 import os
 import webbrowser
 
+os.system('start cmd /k py Server/server.py /min')
 #open child directory React
 os.chdir('React')
 #run npm install if needed
 os.system('npm install')
-
-os.system('start cmd /k py server.py /min')
 
 webbrowser.open('http://localhost:5173')
 os.system('npm run dev')
