ba1f1e679b279840f9c421c319b14ef3bdcfd660
nowa <57222741+nowaaaaaa@users.noreply.github.com>
2023-06-13 14:10:36 +0200

    netter zo

--- Server/server.py
+++ Server/server.py
@@ -1,48 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
-
-def compareVersions(version, start, end):
-    less = False
-    more = False
-    print(version, start, end)
-    version = splitVersion(version)
-    if (start == "any"):
-        more = True
-    else:
-        start = splitVersion(start)
-    if (end == "any"):
-        less = True
-    else:
-        end = splitVersion(end)
-    if (version == []):
-        return False
-    print(version, start, end)
-    for i in range(len(version)):
-        if not more and ((i == len(version)-1 and len(version) < len(start) and version[i] == start[i]) or int(start[i]) > int(version[i])):
-            return False
-        if not less and ((i == len(end)-1 and len(end) < len(version) and version[i] == end[i]) or int(end[i]) < int(version[i])):
-            return False
-        if not more and (int(version[i]) > int(start[i]) or (version[i] == start[i] and len(version) > len(start) and i == len(start)-1)):
-            more = True
-        if not less and (int(version[i]) < int(end[i]) or (version[i] == end[i] and len(version) < len(end) and i == len(version)-1)):
-            less = True
-        if (more and less):
-            return True
-    return True
-
-def splitVersion(version):
-    res = []
-    tempres = ""
-    for i in version:
-        if (i.isdigit()):
-            tempres += i
-        elif tempres != "":
-            res.append(tempres)
-            tempres = ""
-    if tempres != "":
-        res.append(tempres)
-    return res
+import versionCompare as vc
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -93,7 +52,7 @@ def get_dependencies(database, collection, depName, vStart, vEnd):
             print(dep.get("name"))
             if (depName in dep.get("name") or depName == "any"):
                 print("found dep")
-                if (compareVersions(dep.get("version"), vStart, vEnd)):
+                if (vc.compareVersions(dep.get("version"), vStart, vEnd)):
                     result = {
                         "name": dep.get("name"),
                         "version": dep.get("version"),
---
+++ Server/versionCompare.py
@@ -0,0 +1,41 @@
+def compareVersions(version, start, end):
+    less = False
+    more = False
+    print(version, start, end)
+    version = splitVersion(version)
+    if (start == "any"):
+        more = True
+    else:
+        start = splitVersion(start)
+    if (end == "any"):
+        less = True
+    else:
+        end = splitVersion(end)
+    if (version == []):
+        return False
+    print(version, start, end)
+    for i in range(len(version)):
+        if not more and ((i == len(version)-1 and len(version) < len(start) and version[i] == start[i]) or int(start[i]) > int(version[i])):
+            return False
+        if not less and ((i == len(end)-1 and len(end) < len(version) and version[i] == end[i]) or int(end[i]) < int(version[i])):
+            return False
+        if not more and (int(version[i]) > int(start[i]) or (version[i] == start[i] and len(version) > len(start) and i == len(start)-1)):
+            more = True
+        if not less and (int(version[i]) < int(end[i]) or (version[i] == end[i] and len(version) < len(end) and i == len(version)-1)):
+            less = True
+        if (more and less):
+            return True
+    return True
+
+def splitVersion(version):
+    res = []
+    tempres = ""
+    for i in version:
+        if (i.isdigit()):
+            tempres += i
+        elif tempres != "":
+            res.append(tempres)
+            tempres = ""
+    if tempres != "":
+        res.append(tempres)
+    return res
