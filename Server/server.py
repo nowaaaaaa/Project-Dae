26631308ae51d5738cf3ab7ae3a476ffccfc4fda
nowa <57222741+nowaaaaaa@users.noreply.github.com>
2023-06-13 14:08:44 +0200

    versieranges werkt

--- Server/server.py
+++ Server/server.py
@@ -2,6 +2,48 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
 
+def compareVersions(version, start, end):
+    less = False
+    more = False
+    print(version, start, end)
+    version = splitVersion(version)
+    if (start == "any"):
+        more = True
+    else:
+        start = splitVersion(start)
+    if (end == "any"):
+        less = True
+    else:
+        end = splitVersion(end)
+    if (version == []):
+        return False
+    print(version, start, end)
+    for i in range(len(version)):
+        if not more and ((i == len(version)-1 and len(version) < len(start) and version[i] == start[i]) or int(start[i]) > int(version[i])):
+            return False
+        if not less and ((i == len(end)-1 and len(end) < len(version) and version[i] == end[i]) or int(end[i]) < int(version[i])):
+            return False
+        if not more and (int(version[i]) > int(start[i]) or (version[i] == start[i] and len(version) > len(start) and i == len(start)-1)):
+            more = True
+        if not less and (int(version[i]) < int(end[i]) or (version[i] == end[i] and len(version) < len(end) and i == len(version)-1)):
+            less = True
+        if (more and less):
+            return True
+    return True
+
+def splitVersion(version):
+    res = []
+    tempres = ""
+    for i in version:
+        if (i.isdigit()):
+            tempres += i
+        elif tempres != "":
+            res.append(tempres)
+            tempres = ""
+    if tempres != "":
+        res.append(tempres)
+    return res
+
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
@@ -37,7 +79,7 @@ def get_collections(database):
 def get_dependencies(database, collection, depName, vStart, vEnd):
     # Connect to the MongoDB server
     client = MongoClient(uri)
-
+    print(depName, vStart, vEnd)
     db = client[database]
 
     collection = db[collection]
@@ -48,7 +90,9 @@ def get_dependencies(database, collection, depName, vStart, vEnd):
         deps = image.get("dependencies")
         foundDeps.append({"dependencies": [], "name": image.get("name")})
         for dep in deps:
+            print(dep.get("name"))
             if (depName in dep.get("name") or depName == "any"):
+                print("found dep")
                 if (compareVersions(dep.get("version"), vStart, vEnd)):
                     result = {
                         "name": dep.get("name"),
@@ -63,42 +107,3 @@ def get_dependencies(database, collection, depName, vStart, vEnd):
 if __name__ == '__main__':
     app.run()
 
-def compareVersions(version, start, end):
-    less = False
-    more = False
-    version = splitVersion(version)
-    if (start == "any"):
-        more = True
-    else:
-        start = splitVersion(start)
-    if (end == "any"):
-        less = True
-    else:
-        end = splitVersion(end)
-    if (version == []):
-        return False
-    for i in range(len(version)):
-        if not more and ((i == len(version)-1 and len(version) < len(start) and version[i] == start[i]) or int(start[i]) > int(version[i])):
-            return False
-        if not less and ((i == len(end)-1 and len(end) < len(version) and version[i] == end[i]) or int(end[i]) < int(version[i])):
-            return False
-        if not more and (int(version[i]) > int(start[i]) or (version[i] == start[i] and len(version) > len(start) and i == len(start)-1)):
-            more = True
-        if not less and (int(version[i]) < int(end[i]) or (version[i] == end[i] and len(version) < len(end) and i == len(version)-1)):
-            less = True
-        if (more and less):
-            return True
-    return True
-
-def splitVersion(version):
-    res = []
-    tempres = ""
-    for i in version:
-        if (i.isdigit()):
-            tempres += i
-        elif tempres != "":
-            res.append(tempres)
-            tempres = ""
-    if tempres != "":
-        res.append(tempres)
-    return res
